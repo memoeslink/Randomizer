6841626cbe48d91af4d11624d2f730660a880f35
memoeslink <memocad@gmail.com>
2021-11-09 21:31:17 -0600

    Added files to repo.

---
+++ src/main/java/java/main/common/WeightedChar.java
@@ -0,0 +1,32 @@
+package java.main.common;
+
+public class WeightedChar {
+    private char value;
+    private double weight;
+
+    public WeightedChar() {
+        value = '\0';
+        weight = 0.0D;
+    }
+
+    public WeightedChar(char value, double weight) {
+        this.value = value;
+        this.weight = weight;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public void setValue(char value) {
+        this.value = value;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
