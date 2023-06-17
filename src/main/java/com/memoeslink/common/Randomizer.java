1d0b06c91e84c9d69eb378eb3762ac6410ef66ef
memoeslink <memocad@gmail.com>
2023-06-16 19:01:17 -0500

    Refactored code.

--- src/main/java/com/memoeslink/common/Randomizer.java
+++ src/main/java/com/memoeslink/common/Randomizer.java
@@ -32,7 +32,13 @@ public class Randomizer {
         bindSeed(null);
     }
 
+    public int getInt() {
+        return r.nextInt();
+    }
+
     public int getInt(int n) {
+        if (n < 1)
+            n = 1;
         return r.nextInt(n);
     }
 
@@ -64,16 +70,32 @@ public class Randomizer {
         return r.nextFloat();
     }
 
+    public float getFloat(int n) {
+        if (n < 1)
+            n = 1;
+        return r.nextFloat(n);
+    }
+
     public float getFloat(float min, float max) {
-        return (r.nextFloat() * (max - min)) + min;
+        if (max > min)
+            return (r.nextFloat() * (max - min)) + min;
+        return r.nextFloat();
     }
 
     public double getDouble() {
         return r.nextDouble();
     }
 
+    public double getDouble(int n) {
+        if (n < 1)
+            n = 1;
+        return r.nextDouble(n);
+    }
+
     public double getDouble(double min, double max) {
-        return (r.nextDouble() * (max - min)) + min;
+        if (max > min)
+            return (r.nextDouble() * (max - min)) + min;
+        return r.nextDouble();
     }
 
     public long getLong() {
@@ -83,7 +105,7 @@ public class Randomizer {
     public long getLong(long n) {
         if (n < 1L)
             n = 1L;
-        return (long) (r.nextDouble() * n);
+        return r.nextLong(n);
     }
 
     public int getGaussianInt(int standardDeviation, int mean, int constraint) {
